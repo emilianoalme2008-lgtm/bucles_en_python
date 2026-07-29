1cf36ffb7b6fac535dd5095f29c7b56c14399532
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-28 21:03:18 -0600

    Create secuencia_cuadrados.py

---
+++ secuencia_cuadrados.py
@@ -0,0 +1,17 @@
+>>> #Secuencia de cuadrados
+... N = int(input("Número positivo: "))
+... i = 1
+... while True:
+...     print(i ** 2)
+...     i += 1
+...     if i > N:
+...         break
+... print("Secuencia de cuadrados hasta", N)
+...
+Número positivo: 5
+1
+4
+9
+16
+25
+Secuencia de cuadrados hasta 5
