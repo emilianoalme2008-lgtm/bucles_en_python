590504bd4ffec1941d780df2ada20803c978eb11
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-28 21:11:46 -0600

    Implement factorial calculation in numero_factorial.py

---
+++ numero_factorial.py
@@ -0,0 +1,51 @@
+>>> #Factorial
+... num = int(input("Número para factorial: "))
+... factorial = 1
+... if num < 0:
+...     print("Factorial no definido para negativos")
+... else:
+...     for i in range(1, num + 1):
+...         factorial *= i
+...     print("El factorial de", num, "es:", factorial)
+...
+Número para factorial: 0
+El factorial de 0 es: 1
+>>>
+>>> #Factorial
+... num = int(input("Número para factorial: "))
+... factorial = 1
+... if num < 0:
+...     print("Factorial no definido para negativos")
+... else:
+...     for i in range(1, num + 1):
+...         factorial *= i
+...     print("El factorial de", num, "es:", factorial)
+...
+Número para factorial: 5
+El factorial de 5 es: 120
+>>>
+>>> #Factorial
+... num = int(input("Número para factorial: "))
+... factorial = 1
+... if num < 0:
+...     print("Factorial no definido para negativos")
+... else:
+...     for i in range(1, num + 1):
+...         factorial *= i
+...     print("El factorial de", num, "es:", factorial)
+...
+Número para factorial: 7
+El factorial de 7 es: 5040
+>>>
+>>> #Factorial
+... num = int(input("Número para factorial: "))
+... factorial = 1
+... if num < 0:
+...     print("Factorial no definido para negativos")
+... else:
+...     for i in range(1, num + 1):
+...         factorial *= i
+...     print("El factorial de", num, "es:", factorial)
+...
+Número para factorial: -3
+Factorial no definido para negativos
