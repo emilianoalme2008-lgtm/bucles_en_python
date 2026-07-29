db138719e03a2021ddc596fd9a7f4899cd31cef8
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-28 21:10:19 -0600

    Add script to count occurrences of letter 'a'

---
+++ contar_letras.py
@@ -0,0 +1,32 @@
+>>> #Contar letras a
+... palabra = input("Ingrese una palabra: ").lower()
+... contador = 0
+... for letra in palabra:
+...     if letra == 'a':
+...         contador += 1
+... print("La letra 'a' aparece", contador, "veces")
+...
+Ingrese una palabra: algoritmo
+La letra 'a' aparece 1 veces
+>>>
+>>> #Contar letras a
+... palabra = input("Ingrese una palabra: ").lower()
+... contador = 0
+... for letra in palabra:
+...     if letra == 'a':
+...         contador += 1
+... print("La letra 'a' aparece", contador, "veces")
+...
+Ingrese una palabra: casa
+La letra 'a' aparece 2 veces
+>>>
+>>> #Contar letras a
+... palabra = input("Ingrese una palabra: ").lower()
+... contador = 0
+... for letra in palabra:
+...     if letra == 'a':
+...         contador += 1
+... print("La letra 'a' aparece", contador, "veces")
+...
+Ingrese una palabra: AAaa
+La letra 'a' aparece 4 veces
