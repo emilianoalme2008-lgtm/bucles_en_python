706263b807d4d894f4ca589c39b7049b508ccf41
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-28 20:56:04 -0600

    Add odd number counter in numeros_impares.py

---
+++ numeros_impares.py
@@ -0,0 +1,29 @@
+>>> #Contador de números impares
+... N = int(input("Número positivo: "))
+... i = 1
+... while True:
+...     if i % 2 != 0:
+...         print(i, end=" ")
+...     i += 1
+...     if i > N:
+...         break
+... print("\nFin. Se mostraron los impares hasta", N)
+...
+Número positivo: 5
+1 3 5
+Fin. Se mostraron los impares hasta 5
+>>>
+>>> #Contador de números impares
+... N = int(input("Número positivo: "))
+... i = 1
+... while True:
+...     if i % 2 != 0:
+...         print(i, end=" ")
+...     i += 1
+...     if i > N:
+...         break
+... print("\nFin. Se mostraron los impares hasta", N)
+...
+Número positivo: 10
+1 3 5 7 9
+Fin. Se mostraron los impares hasta 10
